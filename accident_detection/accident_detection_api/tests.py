ab409df0f92def34b9ad1620e4f7224cd74e96f3
Nyasha Chizampeni <nchizampeni@gmail.com>
2023-01-15 13:27:10 +0200

    ci/cd

--- accident_detection/accident_detection_api/tests.py
+++ accident_detection/accident_detection_api/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
-
+from accident_detection_api.models.responders import Responder
 # Create your tests here.
+
+class ResponderTestCase(TestCase):
+    def setUp(self):
+        Responder.objects.create(first_name="Test name", last_name="Test surname",longitude="40.714", latitude="-74.006")
+    
+    
+    def test_is_responder_active(self):
+        """test if responder is active"""
+        responder = Responder.objects.get(first_name="Test name")
+            
+        self.assertEqual(responder.is_active, True)
